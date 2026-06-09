bcc6a4908e2e75c323e173635caeeb8d9069d785
zkn.lucasxz <lucas.zknz@gmail.com>
2026-06-09 19:59:48 -0300

    configuração da oficina e controles

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 
 const express = require('express');
 const connectDB = require('./src/config/Database');
-const routes = require('./src/routes'); // 1. Importa o arquivo de rotas
+const routes = require('./src/routes'); 
 
 const app = express();
 
 connectDB();
 
 app.use(express.json());
-app.use(routes); // 2. Avisa o Express para usar as rotas criadas
+app.use(routes); 
 
 const PORT = 3000;
 app.listen(PORT, () => {
